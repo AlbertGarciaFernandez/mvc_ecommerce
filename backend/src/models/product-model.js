f52748661b7444954cb13ba446b4e121b2b52486
ibaialberdi <ibaialberdi@gmail.com>
2022-03-28 17:39:26 +0200

    BACK: added product model

---
+++ backend/src/models/product-model.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+
+const ProductSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  description: {
+    type: String,
+    trim: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  unitsInStock: {
+    type: Number,
+    default: 0,
+  },
+  starValue: {
+    type: Number,
+    required: true,
+  },
+  images: {
+    main: String,
+    others: Array,
+  },
+});
+
+const ProductSchema = new mongoose.model("product", ProductSchema);
+
+module.exports = ProductModel;
