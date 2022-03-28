85415044765572686b73fc4e4b487049849ca63f
Albert Garcia Fernandez <alberd_beto@hotmail.com>
2022-03-28 11:27:28 +0200

    db employee add

---
+++ backend/src/db/employee.js
@@ -0,0 +1,33 @@
+function getEmployees () {
+// we have to hash passwords
+    return [
+        {
+            fullName:'Albert Garcia',
+            email: 'albertgarcia@devstore.es',
+            password: 123,
+            profileImage: 'https://static.wikia.nocookie.net/thebigbangtheory/images/f/fa/SheldonCooper.jpg/revision/latest/scale-to-width-down/562?cb=20160118193007&path-prefix=es',
+            admin: true,
+        },
+        {
+            fullName:'Ibai Iberdi',
+            email: 'ibaialberdi@devstore.es',
+            password: 123,
+            profileImage: 'https://static.wikia.nocookie.net/thebigbangtheory/images/f/fa/SheldonCooper.jpg/revision/latest/scale-to-width-down/562?cb=20160118193007&path-prefix=es',
+            admin: true,
+        },
+        {
+            fullName:'Ivan Gunchev',
+            email: 'ivangunchev@devstore.es',
+            password: 123,
+            profileImage: 'https://static.wikia.nocookie.net/thebigbangtheory/images/f/fa/SheldonCooper.jpg/revision/latest/scale-to-width-down/562?cb=20160118193007&path-prefix=es',
+            admin: false,
+        },
+        {
+            fullName:'Luis Molina',
+            email: 'luismolina@devstore.es',
+            password: 123,
+            profileImage: 'https://static.wikia.nocookie.net/thebigbangtheory/images/f/fa/SheldonCooper.jpg/revision/latest/scale-to-width-down/562?cb=20160118193007&path-prefix=es',
+            admin: false,
+        },
+    ]
+}
