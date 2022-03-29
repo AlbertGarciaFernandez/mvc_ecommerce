b1c753ba84f198feecccd36b6805adf4fec615b3
ibaialberdi <ibaialberdi@gmail.com>
2022-03-29 15:48:20 +0200

    "BACK: Created User Model"

---
+++ backend/src/models/user-model.js
@@ -0,0 +1,37 @@
+const mongoose = require("mongoose");
+const validator = require("validator");
+
+const UserSchema = new mongoose.Schema(
+  {
+    _id: String,
+    firstName: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    lastName: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: [true, "The email is required"],
+      trim: true,
+      unique: true,
+      validate: {
+        validator: (value) => validator.isEmail(value),
+        message: (props) => `The email ${props.value} is not valid`,
+      },
+    },
+    admin: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { timestamps: true }
+);
+
+const UserModel = new mongoose.model("user", UserSchema);
+
+module.exports = UserModel;
